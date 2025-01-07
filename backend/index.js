6894892f5a2d5e77fa9df7a993f7c35cee3b7892
ilyas-ilz <xbeatlover@gmail.com>
2025-01-07 09:19:23 +0530

    sign up functionality

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,13 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv"; 
 import userRouter from './routes/user.route.js';
+import  authRouter from './routes/auth.route.js';
+
 dotenv.config();
 
 const app = express();
+
+app.use(express.json());
 
 mongoose.connect(process.env.MONGO)
     .then(()=>{console.log("Connected Mongo");})
@@ -17,3 +21,16 @@ app.listen(3000, () => {
 
 
 app.use("/api/user",userRouter);
+app.use("/api/auth",authRouter);
+
+app.use((err,req,res,next)=>{
+     const statusCode = err.statusCode || 500;
+     const message = err.message || 'Internal Server Error';
+     return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+
+     });
+
+}) ;    
