aab8be2d2eddfecd01abfc798facc340567e6282
henna-maryam <hennamaryam02@gmail.com>
2025-01-12 22:30:55 +0530

    cleared mistake

--- frontend/src/redux/user/userSlice.js
+++ frontend/src/redux/user/userSlice.js
@@ -10,7 +10,7 @@ const userSlice = createSlice({
     name: 'user',
     initialState,
     reducers: {
-        signInStart: (start) => {
+        signInStart: (state) => {
             state.loading = true; 
         },
         SignInSuccess: (state,action) => {
